59f0ae8c66ec1067af6af4633c865d7ab37d502b
Newland-A <ajn252@gmail.com>
2020-08-26 17:40:27 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,8 @@
+require 'pry'
 #spec/cash_register_spec.rb test file with
+
 class CashRegister
-  attr_accessor :total, :discount, :price, :items
+  attr_accessor :total, :discount, :price, :items, :last_transaction_amount
 #need @ to access instance variable
 #if it is a local variable no @
 #sets a default for dicount to be 0
@@ -15,8 +17,7 @@ class CashRegister
 #add_items with aruguments items, price, quantity
   def add_item(title, price, quantity = 1)
     #set instance price to equal price
-    @price = price
-    
+    self.total += (price * quantity)
     #if quantity is greater than 1
     if quantity > 1
       #sets a counter for the amount
@@ -33,20 +34,18 @@ class CashRegister
       @items << title
     end
     #Calculates the total for price * the quanity of item
-    @total += price * quantity
     #sets the last transaction amount to equal total
-    @last_transaction_amount = @total
-    #returns total
-    @total
+    self.last_transaction_amount = price * quantity
+   
   end
   
   def apply_discount
     #if the dicount is > 0
     if @discount > 0
       #it takes off the amount of the discount 
-      @to_take_off = (price * discount)/ 100.to_f
+      @discount = @discount/100.to_f
       #total minus the amount of discount
-      @total -= @to_take_off
+      @total = @total - (@total * (@discount))
       #returns message
       "After the discount, the total comes to $#{total.to_i}."
     else 
@@ -57,29 +56,7 @@ class CashRegister
 
   def void_last_transaction
     #Sutracts the last item from the total
-    @total -= @last_transaction_amount
+   self.total = self.total - self.last_transaction_amount
+  
   end
 end
-
-# def apply_discount
-#   if @discount > 0
-#     @to_take_off = (price * discount)/100
-#     @total -= @to_take_off
-#     return "After the discount, the total comes to $#{total}."
-#   else
-#     return "There is no discount to apply."
-#   end
-# end
-
-# def add_item(item, price, quantity = 1)
-#   @price = price
-#   @total += price * quantity
-#   if quantity > 1
-#     counter = 0
-#     while counter < quantity
-#       @items << item
-#       counter += 1
-#     end
-#   else
-#     @items << item
-#   end
